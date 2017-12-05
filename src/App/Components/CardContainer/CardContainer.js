7822bfcd73ba7995736b0d590fc5a1b6716c298e
Thomas Laird <t6r6l5@gmail.com>
2017-12-04 22:00:41 -0700

    conditional rendering for cards/placeholder

--- src/App/Components/CardContainer/CardContainer.js
+++ src/App/Components/CardContainer/CardContainer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './CardContainer.css';
+import Card from '../Card/Card';
 
 const CardContainer = (props) => {
-
+  const placeholder = props.cards ? null : (<h3>select a category</h3>);
+  const mappedCards = props.cards ? props.cards.map( card => {
+    return <Card />;
+  }) : null;
   return (
     <div className="CardContainer">
-      card container
+      {placeholder}
+      {mappedCards}
     </div>
   );
 }
