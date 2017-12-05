4b9f8c86377961f2ba279be16086b4addfc8644c
Thomas Laird <t6r6l5@gmail.com>
2017-12-04 18:58:53 -0700

    load and populate film data and pass to filmtext componeent

--- src/App/App.js
+++ src/App/App.js
@@ -6,14 +6,48 @@ import Controls from './Components/Controls/Controls';
 import CardContainer from './Components/CardContainer/CardContainer';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filmName: '',
+      filmYear: '',
+      filmText: '',
+      numFavorites: 5,
+      filmTextOpen: true
+    }
+  }
+
+  componentDidMount() {
+    // fetch('https://swapi.co/api/films/1/')
+    //   .then(response => response.json())
+    //     .then(data => {
+    //       const filmName = data.title;
+    //       const filmYear = data.release_date;
+    //       const filmText = data.opening_crawl;
+
+    //       this.setState({
+    //         filmName, filmYear, filmText
+    //       });
+    //     })
+  }
+
+  closeFilmText = () => {
+    
+  }
+
+
   render() {
     return (
       <div className="App">
         <div className="left-container">
-          <FilmText />
+          <FilmText    
+          filmName = {this.state.filmName}
+          filmYear = {this.state.filmYear}
+          filmText = {this.state.filmText}
+          filmTextOpen = {this.state.filmTextOpen} />
         </div>
         <div className="right-container">
-          <Header />
+          <Header numFavorites={this.state.numFavorites}/>
           <Controls />
           <CardContainer />
         </div>
