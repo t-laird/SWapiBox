f5c9d2aa9dccf8b099914d2cd617f7f4f1aa4510
Thomas Laird <t6r6l5@gmail.com>
2017-12-07 13:24:10 -0700

    mock local storage in app test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import mockApiResponse from './mockApiResponse';
+import { shallow, mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+global.localStorage = {
+  getItem: function(query){return JSON.stringify(mockApiResponse[query]);},
+  setItem: function(){}
+};
+
+describe('App shallow tests', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  it('should mount with the correct components', () => {
+    const renderedApp = shallow(<App />);
+  });
+
 });
