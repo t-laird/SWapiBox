817aba4d9e3b71a5b5d0e47d60f87ca5bf8b6bff
Thomas Laird <t6r6l5@gmail.com>
2017-12-07 10:50:03 -0700

    refactor filmfetch to pull all 7 films

--- src/App/Components/FilmText/FilmText.js
+++ src/App/Components/FilmText/FilmText.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './FilmText.css';
 import SWLogo from '../../../Assets/SWlogo.png';
 import episodeI from '../../../Assets/starwarsI.png';
@@ -19,28 +19,59 @@ const posters = {
   VII: episodeVII,
 }
 
-const FilmText = (props) => {
-  console.log(props);
-  return (
-    <div className="FilmText">
-      <div className="logo-container">
-        <img src={SWLogo} alt="StarWars Logo" />
-      </div>
-      <div className="openingText">
-        <img src={posters[props.filmData.episode]} alt="starwars-poster" />
-        <div className="text-container">
-          <h1>Episode {props.filmData.episode}</h1>
-          <h2>{props.filmData.filmName.toUpperCase()}</h2>
-          {props.filmData.filmText[0]}<br /> <br /> 
-          {props.filmData.filmText[1]}<br /> <br />    
-          {props.filmData.filmText[2]}<br />  <br />
-        </div>
+class FilmText extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rawData: props.filmData
+    };
+  }
+
+  componentWillMount() {
+    if (this.state.rawData.length > 0) {
+      const splitTextRegex = new RegExp(/\s{3}/, 'g');
+      const randomNum = Math.floor(Math.random() * 7);
+      const convertNumToNumeral = {1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII'};
       
+      const randomFilm = this.state.rawData[randomNum];
+      const splitTextArray = randomFilm.opening_crawl.split(splitTextRegex);    
+  
+      const filmName = randomFilm.title;
+      const filmYear = randomFilm.release_date;
+      const filmText = splitTextArray;
+      const episodeNumber = randomFilm.episode_id;
+  
+      const numeral = convertNumToNumeral[episodeNumber];
+  
+      this.setState({
+        filmText, filmName, filmYear, numeral
+      });
+    }
+  }
+
+  render() {
+    const {numeral, filmName, filmText, filmYear} = this.state;
+    return (
+      <div className="FilmText">
+        <div className="logo-container">
+          <img src={SWLogo} alt="StarWars Logo" />
+        </div>
+        <div className="openingText">
+          <img src={posters[numeral]} alt="starwars-poster" />
+          <div className="text-container">
+            <h1>Episode {numeral}</h1>
+            <h2>{filmName.toUpperCase()}</h2>
+            {filmText[0]}<br /> <br /> 
+            {filmText[1]}<br /> <br />    
+            {filmText[2]}<br />  <br />
+          </div>
+        
+        </div>
+        <h1>{filmName}</h1>
+        <h3>{filmYear}</h3>
       </div>
-      <h1>{props.filmData.filmName}</h1>
-      <h3>{props.filmData.filmYear}</h3>
-    </div>
-  );
+    );
+  }
 }
 
 export default FilmText;
