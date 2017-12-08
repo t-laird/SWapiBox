63aaa35d3fd3d96234186b20c4e074f77e3c61cd
Thomas Laird <t6r6l5@gmail.com>
2017-12-08 13:47:58 -0700

    refactor card to remove repitition

--- src/App/Components/Card/Card.js
+++ src/App/Components/Card/Card.js
@@ -4,105 +4,79 @@ import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 
 const Card = (props) => {
-  const personCard = props.cardData.homeworld ? (
-    <div className="card-container">
-      <div className="card-header">
-        <i className="icon-person"></i>
-        <h2>{props.cardData.name}</h2>
-        <Button 
-          buttonClass="Button" 
-          icon={props.starClass} 
-          favoriteCard={props.favoriteCard}
-          card={props.cardData} />
-      </div>
-      <table>
-        <tbody>
-          <tr>
-            <td className="key-highlight">Species:</td>
-            <td>{props.cardData.species}</td>
-          </tr>
-          <tr>
-            <td className="key-highlight">Homeworld:</td>
-            <td>{props.cardData.homeworld}</td>
-          </tr>
-          <tr>
-            <td className="key-highlight">Population of Homeworld:</td>
-            <td>{props.cardData.population}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+  const personTable = props.cardData.homeworld ? (
+    <tbody>
+      <tr>
+        <td className="key-highlight">Species:</td>
+        <td>{props.cardData.species}</td>
+      </tr>
+      <tr>
+        <td className="key-highlight">Homeworld:</td>
+        <td>{props.cardData.homeworld}</td>
+      </tr>
+      <tr>
+        <td className="key-highlight">Population of Homeworld:</td>
+        <td>{props.cardData.population}</td>
+      </tr>
+    </tbody>
   ) : null;
 
-  const planetCard = props.cardData.terrain? (
-    <div className="card-container">    
-      <div className="card-header">
-        <i className="icon-globe"></i>
-        <h2>{props.cardData.name}</h2>
-        <Button 
-          buttonClass="Button" 
-          icon={props.starClass} 
-          favoriteCard={props.favoriteCard} 
-          card={props.cardData} />
-      </div>
-      <table>
-        <tbody>
-          <tr>
-            <td className="key-highlight">Terrain:</td>
-            <td>{props.cardData.terrain}</td>
-          </tr>
-          <tr>
-            <td className="key-highlight">Population:</td>
-            <td>{props.cardData.population}</td>
-          </tr>
-          <tr>
-            <td className="key-highlight">Climate:</td>
-            <td>{props.cardData.climate}</td>
-          </tr>
-          <tr>
-            <td className="key-highlight">Notable Residents: </td>
-            <td>{props.cardData.residents}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+  const planetTable = props.cardData.terrain? (
+    <tbody>
+      <tr>
+        <td className="key-highlight">Terrain:</td>
+        <td>{props.cardData.terrain}</td>
+      </tr>
+      <tr>
+        <td className="key-highlight">Population:</td>
+        <td>{props.cardData.population}</td>
+      </tr>
+      <tr>
+        <td className="key-highlight">Climate:</td>
+        <td>{props.cardData.climate}</td>
+      </tr>
+      <tr>
+        <td className="key-highlight">Notable Residents: </td>
+        <td>{props.cardData.residents}</td>
+      </tr>
+    </tbody>
   ) : null;
 
-  const vehicleCard = props.cardData.class ? (
-    <div className="card-container">    
-      <div className="card-header">
-        <i className="icon-rocket"></i>
-        <h2>{props.cardData.name}</h2>
-        <Button 
-          buttonClass="Button" 
-          icon={props.starClass} 
-          favoriteCard={props.favoriteCard} 
-          card={props.cardData} />
-      </div>
-      <table>
-        <tbody>
-          <tr>
-            <td className="key-highlight">Model:</td>
-            <td>{props.cardData.model}</td>
-          </tr>
-          <tr>
-            <td className="key-highlight">Class:</td>
-            <td>{props.cardData.class}</td>
-          </tr>
-          <tr>
-            <td className="key-highlight">No. Passengers:</td>
-            <td>{props.cardData.passengers}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+  const vehicleTable = props.cardData.class ? (
+    <tbody>
+      <tr>
+        <td className="key-highlight">Model:</td>
+        <td>{props.cardData.model}</td>
+      </tr>
+      <tr>
+        <td className="key-highlight">Class:</td>
+        <td>{props.cardData.class}</td>
+      </tr>
+      <tr>
+        <td className="key-highlight">No. Passengers:</td>
+        <td>{props.cardData.passengers}</td>
+      </tr>
+    </tbody>
   ) : null;
 
   return (
     <div className="Card">
-      {personCard}
-      {planetCard}
-      {vehicleCard}
+      <div className="card-container">
+        <div className="card-header">
+          <i className="icon-person"></i>
+          <h2>{props.cardData.name}</h2>
+          <Button 
+            buttonClass="Button" 
+            icon={props.starClass} 
+            favoriteCard={props.favoriteCard}
+            card={props.cardData} />
+        </div>
+        <table>
+          {personTable}
+          {planetTable}
+          {vehicleTable}
+        </table>
+      </div>
     </div>
   );
 };
@@ -111,16 +85,6 @@ export default Card;
 
 Card.propTypes = {
   cardData: PropTypes.object,
-  ['cardData.homeworld']: PropTypes.string,
-  ['cardData.name']: PropTypes.string,
-  ['cardData.species']: PropTypes.string,
-  ['cardData.population']: PropTypes.string,
-  ['cardData.terrain']: PropTypes.string,
-  ['cardData.climate']: PropTypes.string,
-  ['cardData.residents']: PropTypes.string,
-  ['cardData.class']: PropTypes.string,
-  ['cardData.model']: PropTypes.string,
-  ['cardData.passengers']: PropTypes.string,
   starClass: PropTypes.string,
   favoriteCard: PropTypes.func
 };
