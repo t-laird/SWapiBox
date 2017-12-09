352845904e81c24b77d9bd0dbb27fa92086141c0
Thomas Laird <t6r6l5@gmail.com>
2017-12-08 21:43:10 -0700

    change conditional card render to a router switch

--- src/App/Components/CardContainer/CardContainer.js
+++ src/App/Components/CardContainer/CardContainer.js
@@ -2,95 +2,32 @@ import React from 'react';
 import './CardContainer.css';
 import Card from '../Card/Card';
 import PropTypes from 'prop-types';
+import People from './People';
+import Planets from './Planets';
+import Favorites from './Favorites';
+import Vehicles from './Vehicles';
+import {
+  Route,
+  Switch
+} from 'react-router-dom';
 
-const CardContainer = (props) => {
-  const displayVehicles = props.currentData === 'vehicles';
-  const displayPeople = props.currentData === 'people';
-  const displayPlanets = props.currentData === 'planets';
-  const displayPlaceholder = props.currentData === null;
-  const displayFavorites = props.currentData === 'View Favorites';
 
-  const vehicleCards = displayVehicles 
-    ? props.vehicles.map( (vehicle, index) => {
-      const isFavorited = 
-        props.favorites.find( favorite => vehicle.name === favorite.name);
-      const starClass = isFavorited 
-        ? 'icon-star' 
-        : 'icon-star-empty';
-      
-      return <Card 
-        favoriteCard={props.favoriteCard} 
-        iconType="icon-rocket"
-        starClass={starClass} 
-        cardData={vehicle} 
-        key={index} />;
-    })
-    : null;
-                        
-  const peopleCards = displayPeople
-    ? props.people.map( (person, index) => {
-      const isFavorited = 
-        props.favorites.find( favorite => person.name === favorite.name);
-      const starClass = isFavorited ? 'icon-star' : 'icon-star-empty';
-      return (
-        <Card 
-          favoriteCard={props.favoriteCard}
-          iconType="icon-person" 
-          starClass={starClass} 
-          cardData={person} 
-          key={index} /> );
-    }) 
-    : null;
-                        
-  const planetCards = displayPlanets ?
-    props.planets.map( (planet, index) => {
-      const isFavorited = 
-        props.favorites.find( favorite => planet.name === favorite.name);
-      const starClass = isFavorited ? 'icon-star' : 'icon-star-empty';
-      return (
-        <Card 
-          favoriteCard={props.favoriteCard} 
-          iconType="icon-globe"
-          starClass={starClass} 
-          cardData={planet} 
-          key={index} /> );
-    })
-    : null;
-                        
-  let favoriteCards = displayFavorites ?
-    props.favorites.map( (favorite, index) => {
-      return (
-        <Card 
-          favoriteCard={props.favoriteCard} 
-          starClass={'icon-star'} 
-          cardData={favorite} 
-          key={index} />
-      );
-    }) 
-    : null;
-
-  const favoritesEmpty = displayFavorites && props.favorites.length === 0;
-
-  favoriteCards = favoritesEmpty 
-    ? (<h3>You don't have any favorites yet!</h3>)
-    : favoriteCards;
-                      
-  
-  const placeholder = displayPlaceholder 
-    ? <h3>Click one of the options above to display information</h3>
-    : null; 
-
-              
+const CardContainer = (props) => {             
   return (
-    <div className="CardContainer">
-      <div className="row-wrapper">
-        {placeholder}
-        {vehicleCards}
-        {peopleCards}
-        {planetCards}      
-        {favoriteCards}
-      </div>
-    </div>
+    <Switch>
+      <Route path='/people' render={(routeProps) => (
+        <People favoriteCard={props.favoriteCard} peopleData={props.people} favorites={props.favorites} {...routeProps}/>
+      )} />
+      <Route path='/vehicles' render={(routeProps) => (
+        <Vehicles favoriteCard={props.favoriteCard} vehicleData={props.vehicles} favorites={props.favorites} {...routeProps}/>
+      )} />
+      <Route path='/planets' render={(routeProps) => (
+        <Planets {...routeProps} favoriteCard={props.favoriteCard} planetData={props.planets} favorites={props.favorites}/>
+      )} />
+      <Route path='/favorites' render={(routeProps) => (
+        <Favorites {...routeProps} favoriteCard={props.favoriteCard} favorites={props.favorites} />
+      )} />
+    </Switch>
   );
 };
 
