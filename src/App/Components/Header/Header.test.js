206a99da3ccf4c7b2d450ca79b5c0651a72a32b9
Thomas Laird <t6r6l5@gmail.com>
2017-12-08 23:02:26 -0700

    fix header tests

--- src/App/Components/Header/Header.test.js
+++ src/App/Components/Header/Header.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
 
 
 describe('Header tests', () => {
@@ -9,30 +10,33 @@ describe('Header tests', () => {
   let mockFn;
   beforeEach(() => {
     mockFn = jest.fn();
-    renderedHeader = shallow(
-      <Header 
-        currentData="People"
-        selectData={mockFn}
-        numFavorites={3} />);
+    renderedHeader = mount(
+      <MemoryRouter>
+        <Header 
+          currentData="People"
+          selectData={mockFn}
+          numFavorites={3} />
+      </MemoryRouter>);
   });
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Header />, div);
+    ReactDOM.render(<MemoryRouter><Header /></MemoryRouter>, div);
   });
   it('should render the correct default header elements', () => {
     const expectedH1Length = 1;
-    const expectedButtonLength = 1;
-
+    const expectedButtonLength = 3;
     expect(renderedHeader.find('h1').length).toEqual(expectedH1Length);
-    expect(renderedHeader.find('Button').length).toEqual(expectedButtonLength);
+    expect(renderedHeader.find('.Button').length).toEqual(expectedButtonLength);
   });
   it('should change the number of favorites inside the rendred button depending upon the number passed in via the favorites prop', () => {
     renderedHeader = mount(
-      <Header 
-        currentData="People"
-        selectData={mockFn}
-        numFavorites={3} />);
-    const expectedButtonText = "View Favorites  3";
-    expect(renderedHeader.find('Button').text()).toEqual(expectedButtonText);
+      <MemoryRouter>
+        <Header 
+          currentData="People"
+          selectData={mockFn}
+          numFavorites={3} />
+      </MemoryRouter>);
+    const expectedButtonText = "favorites 3";
+    expect(renderedHeader.find('.Button').at(2).text()).toEqual(expectedButtonText);
   });
 });
